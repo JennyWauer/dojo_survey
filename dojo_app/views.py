0ced04410e5b7e0ed54d1c140f21874be93dbe8d
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-21 11:01:23 -0600

    debug for redirect

--- dojo_app/views.py
+++ dojo_app/views.py
@@ -1,17 +1,23 @@
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
 
-def result(request):
-    if request.method == 'POST':
-        context = {
+def survey(request):
+    if request.method == 'GET':
+        return redirect('/')
+    request.session['survey_result'] = {
             "name": request.POST['name'],
             "location": request.POST['location'],
             "fav": request.POST['fav'],
             "comment": request.POST['comment'],
-        }
-        return render(request, "result.html", context)
-    # return render(request, "result.html")
+    }
+    return redirect('result/')
+
+def result(request):
+    context = {
+        'your_answer': request.session['survey_result']
+    }
+    return render(request, "result.html", context)
